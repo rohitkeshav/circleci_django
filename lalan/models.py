a0b9a6a116b89b7c979a4d1c77791ef757e58cd0
rohit keshav <rohittk239@gmail.com>
2019-02-06 16:26:24 -0500

    ädded models | sample test

--- lalan/models.py
+++ lalan/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, help_text="Enter title", null=False)
+    body = models.CharField(max_length=200, help_text="Add your blog body here", null=False)
+    author = models.CharField(max_length=200, help_text="Enter author name", null=False)
+
+    def __str__(self):
+        return self.title
--- lalan/tests.py
+++ lalan/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from lalan.models import Blog
 
 # Create your tests here.
+
+
+class BlogModelTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Blog.objects.create(title='The 1st blog ever', body='Such blog body, much wow')
+
+    def test_title(self):
+        author = Blog.objects.get(id=1)
+        field_label = author._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'title')
